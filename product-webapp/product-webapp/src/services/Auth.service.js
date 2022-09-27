fa73fa8f8d421a686d706d8a61880dd2180cd14c
dilip <imdilip.patil666@gmail.com>
2022-09-28 00:39:19 +0530

    first commit

---
+++ product-webapp/product-webapp/src/services/Auth.service.js
@@ -0,0 +1,28 @@
+import axios from "axios";
+
+const apiUrl = 'http://3.130.62.114:8080';
+class AuthService {
+
+    // const apiUrl = 'http://localhost:8080';
+    //actual end-points
+    login(data) {
+        return axios.post(`${apiUrl}/authentication/api/v1/user/login`,data)
+    }
+    registerDoctor(data){
+        return axios.post(`${apiUrl}/userservice/api/v1/doctor`,data)
+    }
+    registerPatient(data){
+        return axios.post(`${apiUrl}/userservice/api/v1/patient`,data)
+    }
+
+    //authentication-service end-points
+    
+    // registerDoctor(data) {
+    //     return axios.post('http://localhost:8095/api/v1/user/register',data)
+    // }
+    // login(data) {
+    //     return axios.post('http://localhost:8095/api/v1/user/login',data)
+    // }
+
+}
+export default new AuthService();
